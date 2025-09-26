14855187568e0747b1ad1a0ce0bab10a0879da64
Ago981 <138672137+Ago981@users.noreply.github.com>
2025-09-26 15:58:40 +0200

    Update static.py

--- static/static.py
+++ static/static.py
@@ -1,11 +1,4 @@
-HTML = """
-Don’t care about them all
-Cause all I want is to be loved
-And all I care about is you
-You’re stuck on me like a tattoo
-I don’t care about the pain
-I’ll walk through fire and through rain
-"""
+# static.py
 
 HTML = """
 <!DOCTYPE html>
